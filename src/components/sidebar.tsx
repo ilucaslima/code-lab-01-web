3f0394888d71681838edcbad653d92daf75b40e9
Lucas Lima <lucaslima@MacBook-Pro-de-Lucas.local>
2026-04-18 16:13:17 -0300

    resolver error link next

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link";
+
 export default function Sidebar() {
   return (
     <aside className="h-screen w-64 bg-black text-white flex flex-col p-4">
       <h1 className="text-2xl font-bold mb-6">Meu App</h1>
 
       <nav className="flex flex-col gap-4">
-        <a href="/" className="hover:text-yellow-400">Home</a>
-        <a href="/" className="hover:text-yellow-400"> Uploads</a>
-    
+        <Link href="/" className="hover:text-yellow-400">
+          Home
+        </Link>
+        <Link href="/uploads" className="hover:text-yellow-400">
+          {" "}
+          Uploads
+        </Link>
       </nav>
     </aside>
   );
