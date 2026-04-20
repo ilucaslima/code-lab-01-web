3b0b2f7e9505b0a1e7cfe5df16681020908d0882
Lucas Levingston <lucaolevingston@outlook.com>
2026-04-20 14:38:52 -0300

    Adicionada biblioteca cookies next pra armazenar estados e agora o middleware nao usa mais mock

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,27 @@
-import { type NextRequest, NextResponse } from 'next/server'
-import {
-   authRoutes,
-   DEFAULT_LOGIN_REDIRECT,
-   publicRoutes,
-} from '../routes'
+import { type NextRequest, NextResponse } from "next/server";
+import { authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "../routes";
 
-async function getToken() {
-   return "123"
-}
+export async function middleware(request: NextRequest) {
+   const { nextUrl } = request;
+   const pathname = nextUrl.pathname;
 
-export async function middleware(req: NextRequest) {
-   const { nextUrl } = req
-   const pathname = nextUrl.pathname
+   const token = request.cookies.get("token")?.value;
+   const isLoggedIn = !!token;
 
-   const token = await getToken()
-   const isLoggedIn = !!token
-
-   const isPublicRoute = publicRoutes.includes(pathname)
-   const isAuthRoute = authRoutes.includes(pathname)
+   const isPublicRoute = publicRoutes.includes(pathname);
+   const isAuthRoute = authRoutes.includes(pathname);
 
    if (isAuthRoute && isLoggedIn) {
-      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
    }
 
    if (!isLoggedIn && !isPublicRoute && !isAuthRoute) {
-      return NextResponse.redirect(new URL('/', nextUrl))
+      return NextResponse.redirect(new URL("/", nextUrl));
    }
 
-   return NextResponse.next()
+   return NextResponse.next();
 }
 
 export const config = {
-   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)', '/api/:path*'],
-}
+   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)", "/api/:path*"],
+};
